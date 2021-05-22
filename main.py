d109351f495132e870091593f008d78694f1f07a
unknown <c.lechner@htlkrems.at>
2021-05-22 13:05:38 +0200

    added tkinter

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Test")
-print("Test1")
+import tkinter as tk
+from tkinter import Canvas, filedialog, Text
+
+root = tk.Tk()
+#canvas = tk.Canvas(root, height=700, width=700, bg = "#263D42")
+#canvas.pack()
+
+button7 = tk.Button(root, text="7")
+button7.pack()
+button8 = tk.Button(root, text="7")
+button8.pack()
+button9 = tk.Button(root, text="7")
+button9.pack()
+
+root.mainloop()
